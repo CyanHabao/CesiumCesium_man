47761a6cf8f9bce92818337d911f8dcb0069f93b
Cyan <1456640156@qq.com>
2018-01-24 13:51:19 +0800

    The reference files

---
+++ Apps/Sandcastle/gallery/gallery-index.js
@@ -0,0 +1,407 @@
+// This file is automatically rebuilt by the Cesium build process.
+var hello_world_index = 64;
+var gallery_demos = [{
+  "name": "3D Models",
+  "date": 1483461298350,
+  "img": "3D Models.jpg"
+}, {
+  "name": "3D Models Coloring",
+  "date": 1483629897120,
+  "img": "3D Models Coloring.jpg"
+}, {
+  "name": "3D Tiles Adjust Height",
+  "date": 1501267578105,
+  "img": "3D Tiles Adjust Height.jpg"
+}, {
+  "name": "3D Tiles BIM",
+  "date": 1501267578105,
+  "img": "3D Tiles BIM.jpg"
+}, {
+  "name": "3D Tiles Batch Table Hierarchy",
+  "date": 1501267578105,
+  "img": "3D Tiles Batch Table Hierarchy.jpg"
+}, {
+  "name": "3D Tiles Feature Picking",
+  "date": 1501267578105,
+  "img": "3D Tiles Feature Picking.jpg"
+}, {
+  "name": "3D Tiles Feature Styling",
+  "date": 1501267578105,
+  "img": "3D Tiles Feature Styling.jpg"
+}, {
+  "name": "3D Tiles Formats",
+  "date": 1501267578105,
+  "img": "3D Tiles Formats.jpg"
+}, {
+  "name": "3D Tiles Inspector",
+  "date": 1501697911436,
+  "img": "3D Tiles Inspector.jpg"
+}, {
+  "name": "3D Tiles Interactivity",
+  "date": 1504638000846,
+  "img": "3D Tiles Interactivity.jpg"
+}, {
+  "name": "3D Tiles Interior",
+  "date": 1501267578121,
+  "img": "3D Tiles Interior.jpg"
+}, {
+  "name": "3D Tiles Photogrammetry",
+  "date": 1501267578121,
+  "img": "3D Tiles Photogrammetry.jpg"
+}, {
+  "name": "3D Tiles Point Cloud",
+  "date": 1501267578121,
+  "img": "3D Tiles Point Cloud.jpg"
+}, {
+  "name": "3D Tiles Point Cloud Styling",
+  "date": 1501267578121,
+  "img": "3D Tiles Point Cloud Styling.jpg"
+}, {
+  "name": "ArcGIS MapServer",
+  "date": 1475674370891,
+  "img": "ArcGIS MapServer.jpg"
+}, {
+  "name": "ArcticDEM",
+  "date": 1501267578121,
+  "img": "ArcticDEM.jpg"
+}, {
+  "name": "Atmosphere Color",
+  "date": 1483461298365,
+  "img": "Atmosphere Color.jpg"
+}, {
+  "name": "Billboards",
+  "date": 1501267578121,
+  "img": "Billboards.jpg"
+}, {
+  "name": "Box",
+  "date": 1470920924340,
+  "img": "Box.jpg"
+}, {
+  "name": "CZML",
+  "date": 1502381627210,
+  "img": "CZML.jpg"
+}, {
+  "name": "CZML Billboard and Label",
+  "date": 1483629897134,
+  "img": "CZML Billboard and Label.jpg"
+}, {
+  "name": "CZML Box",
+  "date": 1501267578121,
+  "img": "CZML Box.jpg"
+}, {
+  "name": "CZML Circles and Ellipses",
+  "date": 1501267578121,
+  "img": "CZML Circles and Ellipses.jpg"
+}, {
+  "name": "CZML Colors",
+  "date": 1501267578136,
+  "img": "CZML Colors.jpg"
+}, {
+  "name": "CZML Cones and Cylinders",
+  "date": 1501267578136,
+  "img": "CZML Cones and Cylinders.jpg"
+}, {
+  "name": "CZML Corridor",
+  "date": 1501267578136,
+  "img": "CZML Corridor.jpg"
+}, {
+  "name": "CZML Custom Properties",
+  "date": 1501267578136,
+  "img": "CZML Custom Properties.jpg"
+}, {
+  "name": "CZML Model",
+  "date": 1501267578136,
+  "img": "CZML Model.jpg"
+}, {
+  "name": "CZML Model - Node Transformations",
+  "date": 1501267578136,
+  "img": "CZML Model - Node Transformations.jpg"
+}, {
+  "name": "CZML Path",
+  "date": 1501267578136,
+  "img": "CZML Path.jpg"
+}, {
+  "name": "CZML Point",
+  "date": 1501267578136,
+  "img": "CZML Point.jpg"
+}, {
+  "name": "CZML Point - Time Dynamic",
+  "date": 1472225157180,
+  "img": "CZML Point - Time Dynamic.jpg"
+}, {
+  "name": "CZML Polygon",
+  "date": 1501267578136,
+  "img": "CZML Polygon.jpg"
+}, {
+  "name": "CZML Polygon - Interpolating References",
+  "date": 1472225157196,
+  "img": "CZML Polygon - Interpolating References.jpg"
+}, {
+  "name": "CZML Polygon - Intervals, Availability",
+  "date": 1472225157196,
+  "img": "CZML Polygon - Intervals, Availability.jpg"
+}, {
+  "name": "CZML Polyline",
+  "date": 1501267578136,
+  "img": "CZML Polyline.jpg"
+}, {
+  "name": "CZML Position Definitions",
+  "date": 1472225157211,
+  "img": "CZML Position Definitions.jpg"
+}, {
+  "name": "CZML Rectangle",
+  "date": 1501267578136,
+  "img": "CZML Rectangle.jpg"
+}, {
+  "name": "CZML Reference Properties",
+  "date": 1508770629972,
+  "img": "CZML Reference Properties.jpg"
+}, {
+  "name": "CZML Spheres and Ellipsoids",
+  "date": 1501267578136,
+  "img": "CZML Spheres and Ellipsoids.jpg"
+}, {
+  "name": "CZML Wall",
+  "date": 1501267578152,
+  "img": "CZML Wall.jpg"
+}, {
+  "name": "Callback Property",
+  "date": 1501267578152,
+  "img": "Callback Property.jpg"
+}, {
+  "name": "Camera",
+  "date": 1493671964071,
+  "img": "Camera.jpg"
+}, {
+  "name": "Camera Tutorial",
+  "date": 1501267578152,
+  "img": "Camera Tutorial.jpg"
+}, {
+  "name": "Cardboard",
+  "date": 1501267578152,
+  "img": "Cardboard.jpg"
+}, {
+  "name": "Cesium Inspector",
+  "date": 1501267578152,
+  "img": "Cesium Inspector.jpg"
+}, {
+  "name": "Cesium Widget",
+  "date": 1470920924481,
+  "img": "Cesium Widget.jpg"
+}, {
+  "name": "Circles and Ellipses",
+  "date": 1501267578152,
+  "img": "Circles and Ellipses.jpg"
+}, {
+  "name": "Classification",
+  "date": 1508770629977,
+  "img": "Classification.jpg"
+}, {
+  "name": "Classification Types",
+  "date": 1504638012606,
+  "img": "Classification Types.jpg"
+}, {
+  "name": "Clock",
+  "date": 1501267578152,
+  "img": "Clock.jpg"
+}, {
+  "name": "Clustering",
+  "date": 1501267578152,
+  "img": "Clustering.jpg"
+}, {
+  "name": "Corridor",
+  "date": 1501267578152,
+  "img": "Corridor.jpg"
+}, {
+  "name": "Custom DataSource",
+  "date": 1501267578152,
+  "img": "Custom DataSource.jpg"
+}, {
+  "name": "Custom Geocoder",
+  "date": 1508437748155,
+  "img": "Custom Geocoder.jpg"
+}, {
+  "name": "Cylinders and Cones",
+  "date": 1470920924496,
+  "img": "Cylinders and Cones.jpg"
+}, {
+  "name": "Distance Display Conditions",
+  "date": 1478028591584,
+  "img": "Distance Display Conditions.jpg"
+}, {
+  "name": "GeoJSON and TopoJSON",
+  "date": 1501267578168,
+  "img": "GeoJSON and TopoJSON.jpg"
+}, {
+  "name": "GeoJSON simplestyle",
+  "date": 1470920924512,
+  "img": "GeoJSON simplestyle.jpg"
+}, {
+  "name": "Geometry and Appearances",
+  "date": 1501267578168,
+  "img": "Geometry and Appearances.jpg"
+}, {
+  "name": "Google Earth Enterprise",
+  "date": 1501267578168,
+  "img": "Google Earth Enterprise.jpg"
+}, {
+  "name": "Ground Clamping",
+  "date": 1501267578168,
+  "img": "Ground Clamping.jpg"
+}, {
+  "name": "HTML Overlays",
+  "date": 1501267578168,
+  "img": "HTML Overlays.jpg"
+}, {
+  "name": "HeadingPitchRoll",
+  "date": 1501267578168,
+  "img": "HeadingPitchRoll.jpg"
+}, {
+  "name": "Hello World",
+  "date": 1495565488415,
+  "img": "Hello World.jpg"
+}, {
+  "name": "Imagery Adjustment",
+  "date": 1470920924528,
+  "img": "Imagery Adjustment.jpg"
+}, {
+  "name": "Imagery Layers",
+  "date": 1470920924543,
+  "img": "Imagery Layers.jpg"
+}, {
+  "name": "Imagery Layers Manipulation",
+  "date": 1501267578168,
+  "img": "Imagery Layers Manipulation.jpg"
+}, {
+  "name": "Imagery Layers Split",
+  "date": 1493671964181,
+  "img": "Imagery Layers Split.jpg"
+}, {
+  "name": "Imagery Layers Texture Filters",
+  "date": 1508770629980,
+  "img": "Imagery Layers Texture Filters.jpg"
+}, {
+  "name": "Interpolation",
+  "date": 1501267578168,
+  "img": "Interpolation.jpg"
+}, {
+  "name": "KML",
+  "date": 1477408084731,
+  "img": "KML.jpg"
+}, {
+  "name": "KML Tours",
+  "date": 1503679965485,
+  "img": "KML Tours.jpg"
+}, {
+  "name": "Labels",
+  "date": 1508940261747,
+  "img": "Labels.jpg"
+}, {
+  "name": "LocalToFixedFrame",
+  "date": 1501267578168,
+  "img": "LocalToFixedFrame.jpg"
+}, {
+  "name": "Map Pins",
+  "date": 1471009076948,
+  "img": "Map Pins.jpg"
+}, {
+  "name": "Materials",
+  "date": 1493671964243,
+  "img": "Materials.jpg"
+}, {
+  "name": "Offline",
+  "date": 1483629897178,
+  "img": "Offline.jpg"
+}, {
+  "name": "PATerrain",
+  "date": 1478028591599,
+  "img": "PATerrain.jpg"
+}, {
+  "name": "Particle System",
+  "date": 1501267578168,
+  "img": "Particle System.jpg"
+}, {
+  "name": "Particle System Fireworks",
+  "date": 1501267578168,
+  "img": "Particle System Fireworks.jpg"
+}, {
+  "name": "Physically-Based Materials",
+  "date": 1501873579029,
+  "img": "Physically-Based Materials.jpg"
+}, {
+  "name": "Picking",
+  "date": 1493671964259,
+  "img": "Picking.jpg"
+}, {
+  "name": "Points",
+  "date": 1470920924590,
+  "img": "Points.jpg"
+}, {
+  "name": "Polygon",
+  "date": 1501697911452,
+  "img": "Polygon.jpg"
+}, {
+  "name": "Polyline",
+  "date": 1501267578183,
+  "img": "Polyline.jpg"
+}, {
+  "name": "Polyline Dash",
+  "date": 1501267578183,
+  "img": "Polyline Dash.jpg"
+}, {
+  "name": "Polyline Volume",
+  "date": 1470920924590,
+  "img": "Polyline Volume.jpg"
+}, {
+  "name": "Projection",
+  "date": 1501267578183,
+  "img": "Projection.jpg"
+}, {
+  "name": "Rectangle",
+  "date": 1501267578183,
+  "img": "Rectangle.jpg"
+}, {
+  "name": "Rotatable 2D Map",
+  "date": 1478028591604,
+  "img": "Rotatable 2D Map.jpg"
+}, {
+  "name": "Shadows",
+  "date": 1501267578183,
+  "img": "Shadows.jpg"
+}, {
+  "name": "Show or Hide Entities",
+  "date": 1501267578183,
+  "img": "Show or Hide Entities.jpg"
+}, {
+  "name": "Spheres and Ellipsoids",
+  "date": 1470920924621,
+  "img": "Spheres and Ellipsoids.jpg"
+}, {
+  "name": "Star Burst",
+  "date": 1478028591611,
+  "img": "Star Burst.jpg"
+}, {
+  "name": "Terrain",
+  "date": 1501267578183,
+  "img": "Terrain.jpg"
+}, {
+  "name": "Terrain Exaggeration",
+  "date": 1501267578183,
+  "img": "Terrain Exaggeration.jpg"
+}, {
+  "name": "Video",
+  "date": 1504638012610,
+  "img": "Video.jpg"
+}, {
+  "name": "Wall",
+  "date": 1501267578183,
+  "img": "Wall.jpg"
+}, {
+  "name": "Web Map Service (WMS)",
+  "date": 1470920924653,
+  "img": "Web Map Service (WMS).jpg"
+}, {
+  "name": "Web Map Tile Service with Time",
+  "date": 1501267578183,
+  "img": "Web Map Tile Service with Time.jpg"
+}];
